ef60e142aaca3cbc10d3edcaca943bd6ea38090a
Artemswoy <artem.indukhov.kr@gmail.com>
2017-12-24 19:22:47 +0200

    начало

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Сегодня 24.12.2017");
+        System.out.println("Сегодня 24.12.2017");
     }
 }
